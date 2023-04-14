e7031d18d6960e9cd5ea976534ccdc3877d70612
Stephen Griffiths <github@stegriff.co.uk>
2023-04-14 15:51:32 +0100

    init

---
+++ elements.js
@@ -0,0 +1,30 @@
+var ElementFactory = function () {
+    this.ab = "ab";
+    this.gloss = "gloss";
+    this.mnem = "mnem";
+    this.h1 = "h1";
+
+    this.createElement = function (elementType, order, local, foreign) {
+        const el = {
+            "id": cuid(),
+            "elementType": elementType,
+            "order": order,
+            "alignment": 0,
+            "words": [this.createWord()]
+        }
+        console.log("createElement", el);
+        return el;
+    }
+
+    this.createWord = function (local, foreign) {
+        return {
+            "id" : cuid(),
+            "local": local || "",
+            "foreign": foreign || ""
+        }
+    }
+
+    this.initElements = function () {
+        return [this.createElement(this.gloss, 0, "Hello", "Hola")]
+    }
+}
